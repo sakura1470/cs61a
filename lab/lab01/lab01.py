8ec96b960070263f197a8da73d7a0a1bc40e2899
sakura1470 <1476001066@qq.com>
2022-04-26 20:04:26 +0800

    add old files

---
+++ lab/lab01/lab01.py
@@ -0,0 +1,72 @@
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    m = n
+    if k==0:
+        return 1
+    else:
+        for i in range(k-1):
+            m*=(n-1)
+        return m
+
+
+
+def sum_digits(y):
+    """Sum all the digits of y.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    "*** YOUR CODE HERE ***"
+    y=str(y)
+    m=len(y)
+    x=0
+    for n in range(m):
+        x+=y[n]
+    return x
+
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    n=str(n)
+    x=len(n)
+    if x==1:
+        return False
+    else:
+        for y in range(1,x):
+            if n[y-1]==n[y]==8:
+                print(1)
+        
+                
+
