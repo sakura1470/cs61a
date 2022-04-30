323eb7c199140f787177cae78e2137b0bd6162c7
sakura1470 <1476001066@qq.com>
2022-04-30 09:03:34 +0900

    add project cats

---
+++ project/cats/gui_files/precache-manifest.aa15756538ee0b58a115909847bb1cf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c484df2e123c545933afd24a83c50b1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e663642e071350a7fde4",
+    "url": "/static/css/2.df9ef6f4.chunk.css"
+  },
+  {
+    "revision": "d4aca802b2611c264787",
+    "url": "/static/css/main.dbfcd040.chunk.css"
+  },
+  {
+    "revision": "e663642e071350a7fde4",
+    "url": "/static/js/2.ecdaf578.chunk.js"
+  },
+  {
+    "revision": "d4aca802b2611c264787",
+    "url": "/static/js/main.d2065936.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
